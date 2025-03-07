b599338dddf5abb813ab24d4097499ed95941de1
Wisigman <wisanggagah@gmail.com>
2025-03-07 11:33:57 +0700

    update

---
+++ laravel-registrasi/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/register', 'register');
+Route::post('/submit', [RegisterController::class, 'submit']);
